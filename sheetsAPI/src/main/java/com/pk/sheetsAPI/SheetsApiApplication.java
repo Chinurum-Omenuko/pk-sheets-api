1f918d0b4e2fe735d3fd46c6348f578dce978ff8
Chinurum-Omenuko <omen0003@algonquinlive.com>
2025-08-30 03:17:44 -0400

    init project

---
+++ sheetsAPI/src/main/java/com/pk/sheetsAPI/SheetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.pk.sheetsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetsApiApplication.class, args);
+	}
+
+}
---
+++ sheetsAPI/src/test/java/com/pk/sheetsAPI/SheetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.sheetsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
